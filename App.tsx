fff1244459b900810ac04be1b33f807dd41e23e0
ProgrammerAlok <alokpaul5584@gmail.com>
2023-10-04 18:41:40 +0530

    Basic App Study

---
+++ App.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import {
+  View,
+  Text,
+  SafeAreaView,
+  Button
+} from 'react-native'
+
+function App() {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text> Hellow world </Text>
+        <Text> Hellow world </Text>
+        <Text> Hellow world </Text>
+        <Text> Hellow world </Text>
+        <Button title='btn'/>
+      </View>
+    </SafeAreaView>
+  )
+}
+
+export default App
